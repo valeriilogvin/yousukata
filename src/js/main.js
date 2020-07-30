0579b0fd227f8ed1f9f847cec9058ead64880347
valeriilogvin <valerii.logvin@gmail.com>
2020-07-31 01:50:12 +0300

    first block is over from 80%

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
-var asdasd = require('sdasdasd');
+$('.sect1').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows:false,
+    dots:true,
+    dotsClass: 'my-dots',
+    infinite: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    customPaging : function(slider, i) {
+        // var thumb = $(slider.$slides[i]).data();
+        return '<a class="custom-dots">'+'0'+(i+1)+'</a>';
+    },
+});
