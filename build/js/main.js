77a2643b1d5a0ebbf578950c1c190b7b73486e55
valeriilogvin <valerii.logvin@gmail.com>
2020-08-02 16:42:11 +0300

    fix

--- build/js/main.js
+++ build/js/main.js
@@ -1,14 +1,33 @@
+//slick-slider settings
 $('.sect1').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows:false,
-    dots:true,
+    arrows: false,
+    dots: true,
     dotsClass: 'my-dots',
     infinite: false,
     autoplay: true,
     autoplaySpeed: 4000,
-    customPaging : function(slider, i) {
+    customPaging: function (slider, i) {
         // var thumb = $(slider.$slides[i]).data();
-        return '<a class="custom-dots">'+'0'+(i+1)+'</a>';
+        return '<a class="custom-dots">' + '0' + (i + 1) + '</a>';
     },
+});
+
+// $('.sect1 .my-dots:after').animate({right: 20%, width:100},{duration:500, easing: easeOutExpo});
+
+$(document).ready(function () {
+
+    // appearance & hiding of header-top
+    (function($) {
+        function sayHi() {
+            $('.header-top').css({'display' : 'block'});
+        }
+        setTimeout(sayHi, 2000);
+
+        $('.header-top .close').click(function () {
+            $('.header-top').css({'display' : 'none'});
+        });
+    })(jQuery);
+
 });
