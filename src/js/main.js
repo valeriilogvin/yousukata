1e6aa2df450c335d09cd59fb8f0815117f581882
valeriilogvin <valerii.logvin@gmail.com>
2020-10-01 01:11:27 +0300

    sect-faq

--- src/js/main.js
+++ src/js/main.js
@@ -311,3 +311,22 @@ $(".modal-form").each(function() {
         }
     });
 });
+
+// faq
+$(document).ready(function(){
+    $('.pane_header').click(function(){
+        if ($(this).parent().children('div.subitem').is(':visible')){
+            $('div.subitem').slideUp().siblings('normal');
+            $('.pane_header').removeClass("active");
+            $('.pane_header').parent().removeClass("active");
+        }
+        else {
+            $('div.subitem').slideUp().siblings('normal');
+            $('.pane_header').removeClass("active");
+            $('.pane_header').parent().removeClass("active");
+            $(this).toggleClass("active");
+            $(this).parent().children('div.subitem').slideToggle().siblings('normal');
+            $(this).parent().toggleClass("active");
+        }
+    });
+});
